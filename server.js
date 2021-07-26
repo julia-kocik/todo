d09fbd1ada968343df861d6eaed35f2e2c2d38f6
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-26 08:56:40 +0200

    Add io.on 'connection

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const socket = require('socket.io');
+
+const tasks = [];
 
 const app = express();
 
@@ -6,6 +9,28 @@ const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
 
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', () => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    for(let task of tasks) {
+      if(task.id == socket.id) {
+        const index = tasks.indexOf(task);
+        socket.broadcast.emit('removeTask', task);
+        tasks.splice(index, 1);
+      }
+    }
+  });
+});
+
 app.use((req, res) => {
-    res.status(404).send({ message: "Not found..." });
+  res.status(404).send({ message: "Not found..." });
 });
